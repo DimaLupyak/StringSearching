6e75e3c2134769979ee9d89b6df4b0be5a75fad0
Dima Lupyak <dima.lupyak@gmail.com>
2016-11-15 20:07:03 +0200

    Added file loading

---
+++ Logic/DataProvider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public static class DataProvider
+    {
+        static public string GetStringFromFile(string path)
+        {
+            string text;
+            try
+            {
+                StreamReader streamReader = new StreamReader(path);
+                text = streamReader.ReadToEnd();                
+            }
+            catch (Exception e)
+            {
+                text = "" + e.Message;
+            }
+            return text;
+        }
+    }
+}
--- ViewModel/MainWindowViewModel.cs
+++ ViewModel/MainWindowViewModel.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.ObjectModel;
 using Logic;
+using Microsoft.Win32;
+using System.Windows.Input;
+using System.Threading;
+using System;
 
 namespace ViewModel
 {
@@ -44,20 +43,40 @@ namespace ViewModel
         #region Constructor
         public MainWindowViewModel()
         {
+            LoadTextFromFileCommand = new Command(a => LoadTextFromFile());
+            CloseWindowCommand = new Command(arg => Environment.Exit(0));
             SearchControlViewModels = new ObservableCollection<SearchControlViewModel>();
             foreach (var item in SearchAlgorithmsManager.Instance.GetAll())
             {
                 SearchControlViewModels.Add(new SearchControlViewModel(item));
             }
+
         }
         #endregion
-
+        #region Methods
         public void UpdateResults()
         {
+            if (text != null && pattern != null)
             foreach (var item in SearchControlViewModels)
             {
                 item.UpdateResults(text, pattern);
             }
         }
+
+        public void LoadTextFromFile()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "txt files (*.txt)|*.txt";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                Text = DataProvider.GetStringFromFile(openFileDialog.FileName);
+            }
+        }
+        #endregion
+        #region Commands
+
+        public ICommand LoadTextFromFileCommand { get; set; }
+        public ICommand CloseWindowCommand { get; set; }
+        #endregion
     }
 }
